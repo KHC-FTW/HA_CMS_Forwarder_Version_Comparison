4ce36f4a01ebeaca2df845be6de9062f33197ec4
CHAN Ka Hong <khchan744-c@my.cityu.edu.hk>
2025-01-01 22:48:24 +0800

    setup repo and first push

---
+++ src/main/java/ha/crc2Jasper/CMSFunction.java
@@ -0,0 +1,7 @@
+package ha.crc2Jasper;
+
+public class CMSFunction {
+    private String name = "";
+    private String hospCode = "";
+    private String forwarderVersion = "";
+}
---
+++ src/main/java/ha/crc2Jasper/DataUtils.java
@@ -0,0 +1,10 @@
+package ha.crc2Jasper;
+
+public class DataUtils {
+    private DataUtils() {
+    }
+
+    private static final Database db = Database.getInstance();
+
+
+}
